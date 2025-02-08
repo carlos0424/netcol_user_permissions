43df8eb04451f8fdeaf07398a8fb5bd30c1cbd88
Carlos Arbelaez <tu_email@example.com>
2025-02-07 20:08:13 -0500

    Mejoras en permisos y visibilidad de botones

--- app/controllers/issues_controller.rb
+++ app/controllers/issues_controller.rb
@@ -1,7 +1,8 @@
-before_action :check_export_permission, only: [:xls_export_current, :xls_export]
+before_action :check_export_permission, only: [:xls_export, :xls_export_current]
 
 private
 
+# Método que verifica si el usuario tiene permiso para exportar reportes
 def check_export_permission
   unless User.current.can_export_reports?
     render plain: "Acceso denegado", status: 403
